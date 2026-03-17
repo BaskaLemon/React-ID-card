9f3aca73d28a649678ee6e2f0b35dd3ae50b4b8d
erdenebayarbaasandorj89 <erdenebayarbaasandorj89@gmail.com>
2026-03-17 10:55:52 +0800

    added grid

--- src/app/components/ID.jsx
+++ src/app/components/ID.jsx
@@ -1,6 +1,6 @@
-export const ID = () => {
+export const ID = ({ name, gender, email, code }) => {
   return (
-    <div className="m-10">
+    <div className=" h-150 w-100 flex">
       <div className="bg-teal-300 h-125 w-180 rounded-4xl absolute">
         <div className="m-5 ">
           <div className="text-6xl font-sans font-bold text-teal-600 p-5 border-b-6 border-solid border-teal-600">
@@ -10,23 +10,23 @@ export const ID = () => {
         </div>
         <div className="p-5 flex flex-col gap-7">
           <div className="flex">
-            <div>
+            <div className="w-90 ">
               <div>
                 <p className="text-teal-600 font-bold text-2xl">Name</p>
-                <p className="text-3xl">Baasandorj Erdenebayar</p>
+                <p className="text-2xl">{name}</p>
               </div>
               <div>
                 <p className="text-teal-600 font-bold text-2xl">Gender</p>
-                <p className="text-3xl">Male</p>
+                <p className="text-2xl">{gender}</p>
               </div>
               <div>
                 <p className="text-teal-600 font-bold text-2xl">Email</p>
-                <p className="text-3xl">baskaerka763@gmail.com</p>
+                <p className="text-2xl">{email}</p>
               </div>
             </div>
             <div className="relative bottom-20 left-13 flex flex-col justify-center items-center">
               <img src="https://shiftly.co.za/wp-content/uploads/2024/04/Shiftly-ID-illustrations-02-256x300.png" />
-              <p className="m-2 text-2xl">25BS6464646</p>
+              <p className="m-2 text-2xl">{code}</p>
             </div>
           </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,143 @@ import { ID } from "./components/ID";
 
 export default function Home() {
   return (
-    <div className="">
-      <ID />
+    <div
+      className="grid grid-cols-2
+
+    "
+    >
+      <ID
+        name={"Э. Баасандорж"}
+        gender={"Male"}
+        email={"baskaerka763@gmail.com"}
+        code={"25BS65476"}
+      />
+      <ID
+        name={"Т. Нямбаяр"}
+        gender={"Male"}
+        email={"yunoxsama00@gmail.com"}
+        code={"225LP7381"}
+      />
+      <ID
+        name={"Л. Дөлгөөнзул"}
+        gender={"Female"}
+        email={"dolgoonlkhagva@gmail.com"}
+        code={"25BS65476"}
+      />
+      <ID
+        name={"Г. Номин"}
+        gender={"Female"}
+        email={"gnomin200408@gmail.com"}
+        code={"25BS52373"}
+      />
+      <ID
+        name={"Л. Нямочир"}
+        gender={"Male"}
+        email={"nyamochir.lkhamkhvv@gmail.com"}
+        code={"26BS98744"}
+      />
+      <ID
+        name={"М. Азноров"}
+        gender={"Female"}
+        email={"azaamunkhtn@gmail.com"}
+        code={"25LP1286"}
+      />
+      <ID
+        name={"Б. Билэгсайхан"}
+        gender={"Male"}
+        email={"bbek3201@gmail.com"}
+        code={"26BS83263"}
+      />
+      <ID
+        name={"Ш. Батмагнай"}
+        gender={"Male"}
+        email={"sbatmagnai25@gmail.com"}
+        code={"25LP6073"}
+      />
+      <ID
+        name={"Б. Чинзориг"}
+        gender={"Male"}
+        email={"chinzoobatbold11@gmail.com"}
+        code={"26BS13491"}
+      />
+      <ID
+        name={"Б. Гаажадамба"}
+        gender={"Male"}
+        email={"gaajadamba@gmail.com"}
+        code={"26BS25985"}
+      />
+      <ID
+        name={"О. Анударь"}
+        gender={"Female"}
+        email={"anudariochiredene@gmail.com"}
+        code={"26BS80134"}
+      />
+      <ID
+        name={"Б. Мөнх Оргил"}
+        gender={"Male"}
+        email={"suusiibat@gmail.com"}
+        code={"26BS63531"}
+      />
+      <ID
+        name={"К. Өрнүүн"}
+        gender={"Male"}
+        email={"urnuun919@gmail.com"}
+        code={"25BS21438"}
+      />
+      <ID
+        name={"Т. Батхүлэг"}
+        gender={"Male"}
+        email={"t.khuleg@gmail.com"}
+        code={"26BS38301"}
+      />
+      <ID
+        name={"Б. Цолмон"}
+        gender={"Male"}
+        email={"btsolmon.mn@gmail.com"}
+        code={"26BS11043"}
+      />
+      <ID
+        name={"А. Билгүүнтөгс"}
+        gender={"Male"}
+        email={"bilgntgsamarbayr@gmail.com"}
+        code={"26BS10652"}
+      />
+      <ID
+        name={"Ц. Номин Эрдэнэ"}
+        gender={"Female"}
+        email={"nominerdenetsogtbaatar5@gmail.com"}
+        code={"26BS60931"}
+      />
+      <ID
+        name={"Э. Номин Эрдэнэ"}
+        gender={"Female"}
+        email={"ne78693@gmail.com"}
+        code={"26BS42529"}
+      />
+      <ID
+        name={"А. Баярбилэг"}
+        gender={"Male"}
+        email={"bayarbileg.am@gmail.com"}
+        code={"26BS44296"}
+      />
+      <ID
+        name={"М. Темүүлэн"}
+        gender={"Male"}
+        email={"azeroth0017@gmail.com"}
+        code={"26BS99683"}
+      />
+      <ID
+        name={"Б. Баярцогт"}
+        gender={"Male"}
+        email={"bayartsogt9998@gmail.com"}
+        code={"26BS96881"}
+      />
+      <ID
+        name={"Б. Мянганнаст"}
+        gender={"Male"}
+        email={"myngannast.b@gmail.com"}
+        code={"26BS98039"}
+      />
     </div>
   );
 }
